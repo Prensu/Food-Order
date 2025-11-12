75375aabceb7b8ecf4977a9f7708c2376b0df6c4
Prensu <dangolprensu17newar@gmail.com>
2025-11-12 23:29:07 +0545

    Initial Commit

---
+++ admin/manage-admin.php
@@ -0,0 +1,29 @@
+<?php include('partials/menu.php'); ?>
+
+        <!-- Main Content Section Starts -->
+        <div class="main-content">
+            <div class="wrapper">
+                <h1>Manage Admin</h1>
+
+                <table class="tbl-full">
+                    <tr>
+                        <th>S.N.</th>
+                        <th>Full Name</th>
+                        <th>Username</th>
+                        <th>Actions</th>
+                    </tr>
+                    <tr>
+                        <td>1.</td>
+                        <td>Prensu</td>
+                        <td>prensudangol</td>
+                        <td>
+                           Update Admin
+                           Delete Admin
+                        </td>  
+                    </tr> 
+                </table>
+            </div>
+        </div>
+        <!-- Main Content Setion Ends -->
+
+<?php include('partials/footer.php'); ?>
---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">November 2025 All rights reserved, Food House. Developed By - <a href="#"> Prensu Dangol</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
